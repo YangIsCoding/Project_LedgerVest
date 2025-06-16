4f8a900d2f01ff86cbc66ff699473d0bb2719988
yang881122 <pin-yang.chen@duke.edu>
2025-06-16 15:28:35 -0400

    pic

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     description: 'Secure Investments Through Blockchain Technology',
     images: [
       {
-        url: 'https://www.myledgervest.com/_next/image/?url=%2FHeroSection.png&w=1920&q=75',
+        url: 'https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-1bf4-622f-b77c-0a5cc0a2f9cb/raw?se=2025-06-16T20%3A22%3A02Z&sp=r&sv=2024-08-04&sr=b&scid=e53f779e-74bb-5807-b476-0a3976183a9d&skoid=e9d2f8b1-028a-4cff-8eb1-d0e66fbefcca&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-16T01%3A32%3A04Z&ske=2025-06-17T01%3A32%3A04Z&sks=b&skv=2024-08-04&sig=ePs8xBohk067G4w8T2nojM5HuvFSX3U2TNUOMS71xIk%3D',
         width: 1200,
         height: 630,
         alt: 'LedgerVest Preview Image',
